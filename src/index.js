54dd75e2f981ffccd7f36f99d92ad41b3203ae74
William Gelhar <william.gelhar@validere.com>
2019-04-04 13:28:47 -0400

    Adding helpers to let you add drawn objects to a controlled `value`

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 import SketchField from './SketchField'
 import Tools from './tools'
+import ValueHelpers from './ValueHelpers'
 
 export {SketchField}
 export {Tools}
+export {ValueHelpers}
 
 export default {
   SketchField,
-  Tools
+  Tools,
+  ValueHelpers
 };
